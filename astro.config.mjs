9899646807c0239ee03194411c5de8e50835f185
Ryuhei Mori <mori@math.nagoya-u.ac.jp>
2025-04-09 19:40:45 +0900

    revised

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 import yaml from '@rollup/plugin-yaml';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ryuhei-mori.github.io',
   vite: {
-    plugins: [yaml()]
+    plugins: [yaml(), tailwindcss()]
   }
 });

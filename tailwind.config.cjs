8ff2281cf7a6b41b071ea48f2a964a0264ac4b4d
Ryuhei Mori <mori@math.nagoya-u.ac.jp>
2025-04-11 22:36:18 +0900

    revised

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,2 @@
 const colors = require('tailwindcss/colors')
 
-module.exports = {
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {
-      borderColor: ['focus-visible'],
-      opacity: ['disabled'],
-      textColor: ['visited']
-    }
-  }
-}

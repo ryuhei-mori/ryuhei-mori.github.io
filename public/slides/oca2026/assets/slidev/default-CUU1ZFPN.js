1669a2d383cf37a1f400f6c8bc923c1e4bc67d7b
Ryuehi Mori <mori@math.nagoya-u.ac.jp>
2026-09-25 23:34:32 +0900

    revised

---
+++ public/slides/oca2026/assets/slidev/default-CUU1ZFPN.js
@@ -0,0 +1,1 @@
+import{R as e,V as t,y as n}from"../modules/shiki-B4NFgu0j.js";import{vt as r}from"../index-BJmLlzN5.js";var i={},a={class:`slidev-layout default`};function o(r,i){return e(),n(`div`,a,[t(r.$slots,`default`)])}var s=r(i,[[`render`,o]]);export{s as t};
